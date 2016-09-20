d39c3904c427158f6ecbe09f17200c7c0ca6eea2
TheBinaryBee <coolicooli1@hotmail.de>
2016-09-20 18:35:42 +0200

    updated navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 "use strict";
 
-(function () {})();
+(function () {
+    function $(query, multi) {
+        const _document = document;
+
+        return multi ? _document.querySelectorAll(query) : _document.querySelector(query);
+    }
+
+    function initNav() {
+        const $toggle = $(".navbar-toggler");
+        const $nav = $(".navbar-toggleable-xs");
+
+        $toggle.addEventListener("click", () => {
+            $nav.classList.toggle("in");
+        }, false);
+    }
+
+    initNav();
+})();
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,22 @@
 "use strict";
 
 (function() {
+    function $(query, multi) {
+        const _document = document;
 
+        return multi ? _document.querySelectorAll(query) : _document.querySelector(query);
+    }
+
+    function initNav() {
+        const $toggle = $(".navbar-toggler");
+        const $nav = $(".navbar-toggleable-xs");
+
+        $toggle.addEventListener("click", () => {
+            $nav.classList.toggle("in");
+        }, false);
+    }
+
+
+
+    initNav();
 })();
