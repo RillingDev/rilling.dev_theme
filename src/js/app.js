b26e3b110c528327ff9b950a141cbef759e84312
TheBinaryBee <coolicooli1@hotmail.de>
2016-11-05 18:41:34 +0100

    fixed navbar bugs

--- src/js/app.js
+++ src/js/app.js
@@ -9,63 +9,56 @@ const frilling = (function() {
      * @param  {Boolean} multi If the select will match multiple elements
      * @return {NodeList}       NodeList of selcted elements
      */
-    function $(query, multi) {
+    const $ = function(query, multi) {
         return multi ? _document.querySelectorAll(query) : _document.querySelector(query);
-    }
+    };
 
     /**
      * Iterate over NodeList
      * @param  {NodeList}   node NodeList of elements
      * @param  {Function} fn   Function to run
      */
-    function eachNode(node, fn) {
+    const eachNode = function(node, fn) {
         return [].forEach.call(node, fn);
-    }
+    };
 
-    function init(pageType) {
-        const initNav = function() {
-            const $toggle = $(".navbar-toggler");
-            const $nav = $(".navbar-toggleable-xs");
+    const initNav = function() {
+        const $toggle = $(".navbar-toggler");
+        const $nav = $(".navbar-toggleable-xs");
 
-            $toggle.addEventListener("click", () => {
-                $nav.classList.toggle("in");
-            }, false);
-        };
+        $toggle.addEventListener("click", () => {
+            $nav.classList.toggle("in");
+        }, false);
+    };
 
-        const initArticle = function() {
-            const $pre = $("pre", true);
-            const $tables = $("table", true);
+    const initArticle = function() {
+        const $pre = $("pre", true);
+        const $tables = $("table", true);
 
-            //Highlight Code Snippets
-            eachNode($pre, function($e) {
-                const $code = $e.querySelector("code");
-                const lang = $code.className.replace("language-", "");
-                $e.className = lang;
+        //Highlight Code Snippets
+        eachNode($pre, function($e) {
+            const $code = $e.querySelector("code");
+            const lang = $code.className.replace("language-", "");
+            $e.className = lang;
 
-                hljs.highlightBlock($e);
-            });
+            hljs.highlightBlock($e);
+        });
 
-            //Adjust Table classes
-            eachNode($tables, function(e) {
-                e.classList.add("table");
-                e.classList.add("table-bordered");
-                e.classList.add("table-hover");
-                e.outerHTML = '<div class="table-responsive">' + e.outerHTML + '</div>';
-            });
-        };
+        //Adjust Table classes
+        eachNode($tables, function(e) {
+            e.classList.add("table");
+            e.classList.add("table-bordered");
+            e.classList.add("table-hover");
+            e.outerHTML = '<div class="table-responsive">' + e.outerHTML + '</div>';
+        });
+    };
 
-        /*function initAbout() {
-
-        }*/
-
+    const init = function(pageType) {
         initNav();
         if (pageType === "item") {
             initArticle();
         }
-        /*if (pageType === "about") {
-            initAbout();
-        }*/
-    }
+    };
 
     return {
         init: init
