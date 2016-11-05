b810bcccb3516a1279ed4fa2c4207267a485a3d7
TheBinaryBee <coolicooli1@hotmail.de>
2016-11-05 18:12:05 +0100

    updated bootstrap

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,15 +6,12 @@ const tasks = require("gulp-task-loader")("tasks");
 
 gulp.task("watch", function() {
     gulp.watch("./img/*.*", ["favicon"]);
-    gulp.watch(["./scss/bootstrap.scss", "./scss/_variables.scss"], ["bootstrap", "css"]);
-
-    gulp.watch("./scss/**/*.scss", ["css"]);
-    gulp.watch("./**/*.pug", ["html"]);
+    gulp.watch("./scss/**/*.scss", ["bootstrap", "css"]);
     gulp.watch("./js/**/*.js", ["js"]);
 });
 
 
 gulp.task("dev", ["connect", "watch"]);
-gulp.task("build", [ "css", "js"]);
+gulp.task("build", ["css", "js"]);
 gulp.task("dist", ["sync", "bootstrap", "favicon", "build"]);
 gulp.task("default", ["dist"]);
--- src/tasks/css.js
+++ src/tasks/css.js
@@ -6,14 +6,12 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const cssBeautify = require("gulp-cssbeautify");
 
 module.exports = function() {
     gulp.task("css", function() {
         return gulp
             .src("./scss/app.scss")
             .pipe(sass().on("error", sass.logError))
-            .pipe(cssBeautify())
             .pipe(gulp.dest("../css"));
     });
 };
