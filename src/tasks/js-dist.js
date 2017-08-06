9876f3b3ebd5a2f0ff2fe411b71db5ae4523065f
Felix Rilling <contact@f-rilling.com>
2017-08-06 15:36:16 +0200

    updated stack

--- src/tasks/js-dist.js
+++ src/tasks/js-dist.js
@@ -1,38 +1,16 @@
 "use strict";
 
-const rollup = require("rollup");
 const babel = require("babel-core");
 const uglify = require("uglify-es");
-const nodeResolve = require("rollup-plugin-node-resolve");
-const commonjs = require("rollup-plugin-commonjs");
-const packageJson = require("../package.json");
+const bundle = require("./lib/bundle");
 
-const saveOutput = require("./lib/saveOutput");
-const {
-    DIR_SRC,
-    DIR_DIST
-} = require("./lib/constants");
-
-rollup
-    .rollup({
-        entry: `${DIR_SRC}/js/app.js`,
-        plugins: [
-            nodeResolve({
-                jsnext: true,
-                main: true
-            }),
-            commonjs()
-        ]
-    })
-    .catch(err => {
-        console.log(err);
-    })
-    .then(bundle => {
-        const result = bundle.generate({
-            moduleName: packageJson.namespace.module,
-            format: "iife"
-        }).code;
-        const result_min = uglify.minify(babel.transform(result).code).code;
-
-        saveOutput(`${DIR_DIST}/js/app.js`, result_min, "JS:Dist");
-    });
+bundle([{
+    id: "iife",
+    file: "",
+    name: "IIFE:min",
+    fn: code => uglify.minify(
+        babel.transform(code, {
+            compact: false
+        }).code
+    ).code
+}]);
