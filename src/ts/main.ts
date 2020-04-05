e4a19bd56edfb1ddad90eddefec3f7f36bd6db1f
FelixRilling <contact@rilling.dev>
2020-04-05 12:28:00 +0200

    Updated stack/deps.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,5 +3,5 @@ import { init } from "./init";
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 window.rilling = {
-    init
+    init,
 };
--- src/ts/ui/article.ts
+++ src/ts/ui/article.ts
@@ -1,7 +1,7 @@
 const initArticle = (document: Document): void => {
     const $tables = document.querySelectorAll(".article-content > table");
 
-    $tables.forEach($table => {
+    $tables.forEach(($table) => {
         // Manipulate a virtual element instead of the actual one to improve performance
         const $tableClone = <Element>$table.cloneNode(true);
         $tableClone.classList.add("table", "table-bordered", "table-hover");
