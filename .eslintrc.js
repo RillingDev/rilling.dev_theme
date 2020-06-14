ac7d1e2f8ee9715e1e7893e59cbda31587db8825
FelixRilling <contact@rilling.dev>
2020-06-14 11:05:43 +0200

    Updated stack.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,16 @@ module.exports = {
         tsconfigRootDir: __dirname,
         project: ["./tsconfig.json"],
     },
-    env: {
-        "es6": true,
-        "es2017": true,
-        "es2020": true,
-        "shared-node-browser": true,
-    },
-    plugins: ["import", "@typescript-eslint", "prettier"],
+    plugins: ["@typescript-eslint", "prettier", "import"],
     extends: [
         "eslint:recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier",
         "prettier/@typescript-eslint",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
     ],
     rules: {
         "no-shadow": "error",
@@ -30,10 +23,9 @@ module.exports = {
         "no-template-curly-in-string": "warn",
         "array-callback-return": "warn",
         "consistent-return": "warn",
-        "no-else-return": "warn",
         "no-implicit-coercion": "warn",
         "no-dupe-else-if": "warn",
-        "radix": "warn",
+        radix: "warn",
 
         "prettier/prettier": "warn",
 
@@ -44,34 +36,74 @@ module.exports = {
         "import/no-self-import": "error",
         "import/no-default-export": "warn",
         "import/no-webpack-loader-syntax": "warn",
-        "import/no-cycle": "warn",
         "import/no-useless-path-segments": "warn",
         "import/no-mutable-exports": "warn",
 
-        "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-floating-promises": "error",
         "@typescript-eslint/no-throw-literal": "error",
-        "@typescript-eslint/no-base-to-string": "error",
         "@typescript-eslint/no-dynamic-delete": "error",
         "@typescript-eslint/no-implied-eval": "error",
-        "@typescript-eslint/interface-name-prefix": [
-            "warn",
-            {
-                prefixWithI: "never",
-            },
-        ],
+        "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/consistent-type-assertions": [
             "warn",
             {
-                assertionStyle: "angle-bracket",
+                assertionStyle: "as",
             },
         ],
+        "@typescript-eslint/explicit-function-return-type": [
+            "warn",
+            {
+                allowExpressions: true,
+            },
+        ],
+        "@typescript-eslint/no-base-to-string": "warn",
         "@typescript-eslint/no-extra-non-null-assertion": "warn",
         "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
         "@typescript-eslint/no-unnecessary-condition": "warn",
         "@typescript-eslint/prefer-nullish-coalescing": "warn",
         "@typescript-eslint/prefer-optional-chain": "warn",
-        "@typescript-eslint/prefer-readonly": "warn"
+        "@typescript-eslint/prefer-readonly": "warn",
+        "@typescript-eslint/ban-types": [
+            "warn",
+            {
+                types: {
+                    object: false,
+                },
+            },
+        ],
+        "@typescript-eslint/naming-convention": [
+            "warn",
+            {
+                selector: "default",
+                format: ["strictCamelCase"],
+                leadingUnderscore: "forbid",
+                trailingUnderscore: "forbid",
+            },
+            {
+                selector: "variable",
+                format: ["strictCamelCase", "StrictPascalCase", "UPPER_CASE"],
+            },
+            {
+                selector: "property",
+                format: ["strictCamelCase", "StrictPascalCase", "UPPER_CASE"],
+            },
+            { selector: "typeAlias", format: ["StrictPascalCase"] },
+            {
+                selector: "typeParameter",
+                format: ["PascalCase"], // Allow "T", "TValue", "Value" and such
+            },
+            {
+                selector: "interface",
+                format: ["StrictPascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: false,
+                },
+            },
+            { selector: "class", format: ["StrictPascalCase"] },
+            { selector: "enum", format: ["StrictPascalCase"] },
+            { selector: "enumMember", format: ["UPPER_CASE"] },
+        ],
     },
 };
