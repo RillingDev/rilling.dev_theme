e15390b73d5994e33a7202b27ebe35f26b94a74d
FelixRilling <contact@rilling.dev>
2020-07-17 21:59:22 +0200

    Fixed script format.

--- scripts/bootstrapPaginator.js
+++ scripts/bootstrapPaginator.js
@@ -1,19 +1,25 @@
 /*
-* Based on Hexo's paginatorHelper adjusted for some bootstrap stuff.
+ * Based on Hexo's paginatorHelper adjusted for some bootstrap stuff.
  */
 const paginatorHelper = function () {
     const current = this.page.current || 0;
     const total = this.page.total || 1;
-    const base = this.page.base || '';
+    const base = this.page.base || "";
     const format = `${this.config.pagination_dir}/%d/`;
 
-    if (!current) return '';
+    if (!current) {
+        return "";
+    }
 
     const currentPage = `<li class="page-item active"><span class="page-link">${current}</span></li>`;
 
-    const link = i => this.url_for(i === 1 ? base : base + format.replace('%d', i));
+    const link = (i) =>
+        this.url_for(i === 1 ? base : base + format.replace("%d", i));
 
-    const pageLink = i => `<li class="page-item"><a class="page-link" href="${link(i)}">${i}</a></li>`;
+    const pageLink = (i) =>
+        `<li class="page-item"><a class="page-link" href="${link(
+            i
+        )}">${i}</a></li>`;
 
     let result = "";
 
@@ -33,5 +39,4 @@ const paginatorHelper = function () {
     return result;
 };
 
-
-hexo.extend.helper.register('bootstrap_paginator', paginatorHelper);
+hexo.extend.helper.register("bootstrap_paginator", paginatorHelper);
--- scripts/bootstrapTags.js
+++ scripts/bootstrapTags.js
@@ -1,32 +1,32 @@
 const TAG_LIMIT = 3;
 
 const listTagsHelper = function (tags) {
+    if (!tags || !tags.length) {
+        return "";
+    }
 
-    if (!tags || !tags.length) return '';
-
-    let result = '';
+    let result = "";
 
     // Sort the tags
-    tags = tags.sort('name', 1);
+    tags = tags.sort("name", 1);
 
     // Ignore tags with zero posts
-    tags = tags.filter(tag => tag.length);
+    tags = tags.filter((tag) => tag.length);
 
     // Limit the number of tags
     tags = tags.limit(TAG_LIMIT);
 
-    result+="<ul class='meta-tag-list'>";
+    result += "<ul class='meta-tag-list'>";
 
-    tags.forEach(tag=>{
+    tags.forEach((tag) => {
         result += `<li class='meta-tag-item'>`;
-        result +=  tag.name;
-        result += '</li>';
+        result += tag.name;
+        result += "</li>";
     });
 
-    result+="</ul>";
+    result += "</ul>";
 
     return result;
 };
 
-
-hexo.extend.helper.register('bootstrap_tags', listTagsHelper);
+hexo.extend.helper.register("bootstrap_tags", listTagsHelper);
