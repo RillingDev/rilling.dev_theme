fc079598e1a037bc4671d341cd0b06d3167b38bd
FelixRilling <contact@rilling.dev>
2022-12-03 10:29:26 +0100

    Updated deps.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/sprites/solid.svg";
+// import "@fortawesome/fontawesome-free/sprites/solid.svg";
 import "@fortawesome/fontawesome-free/sprites/brands.svg";
 
 import "./scss/main.scss";
