c6071bc2b45f2b10499acf2aff7d5013b629ac16
RillingDev <contact@rilling.dev>
2024-08-10 09:04:15 +0200

    Updated eslint.

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+	{ ignores: ["source/", "scripts/"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	eslintPluginPrettierRecommended,
+	{ rules: { "prettier/prettier": "warn" } },
+);
