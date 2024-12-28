56e34dd856c267d7c3902bd2098ced0a0b65bd09
RillingDev <contact@rilling.dev>
2024-12-28 09:31:27 +0100

    Simplified navbar, fixes #46.

--- src/ts/main.ts
+++
@@ -1,8 +0,0 @@
-import { initNavbar } from "./ui/navbar.ts";
-
-document.addEventListener("DOMContentLoaded", () =>
-	initNavbar(
-		document.getElementById("navbarToggleButton") as HTMLButtonElement,
-		document.getElementById("navbarMenu")!,
-	),
-);
