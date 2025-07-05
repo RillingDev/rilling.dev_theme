0c41f61abf232c9c721e989972026e4409a94897
RillingDev <contact@rilling.dev>
2025-07-05 10:35:38 +0200

    Updated deps.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "modern-normalize"; // TODO: it may make sense to vendor this and only pick what we need
+import "modern-normalize/modern-normalize.css"; // TODO: it may make sense to vendor this and only pick what we need
 import "./scss/main.scss";
 
 import "./ts/main.ts";
